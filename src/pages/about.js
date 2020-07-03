536fd49f9ed3a2c73a09dce4a5188d8410e5b2c6
nithish ravindra <nithishravindra8@gmail.com>
2020-07-03 16:12:26 +0530

    added form

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../layout/Layout'
+import { Link } from 'gatsby'
 
 const Contact = () => (
   <Layout>
@@ -10,10 +11,11 @@ const Contact = () => (
         got into web development. Started with HTML, CSS and Javascript. I've
         picked ReactJS to help me through front-end. I've been also working on
         Backend Utilities like NodeJS, Python3, SQL and MongoDB to enhance
-        Full-Stack services. You can view some of my work in Projects section of
-        this site.
+        Full-Stack services. You can view some of my work in{' '}
+        <Link to="/project">Projects</Link> section of this site.
       </p>
     </div>
+    <div></div>
   </Layout>
 )
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,74 @@ import React from 'react'
 import Layout from '../layout/Layout'
 import SocialLinks from '../data/socialLinks'
 
-const Contact = () => (
-  <Layout>
-    <h2 className="social-heading"> Catch me up online </h2>
-    <SocialLinks className="social-link-position" />
-  </Layout>
-)
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.submitForm = this.submitForm.bind(this)
+    this.state = {
+      status: '',
+    }
+  }
 
-export default Contact
+  render() {
+    const { status } = this.state
+    return (
+      <Layout>
+        <h2 className="social-heading"> Catch me up online </h2>
+        <SocialLinks className="social-link-position" />
+
+        <div className="form-align">
+          <form
+            onSubmit={this.submitForm}
+            action="https://formspree.io/mleppdpr"
+            method="POST"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              class="form-control"
+            ></input>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              class="form-control"
+            />
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="message"
+              class="form-control"
+            ></textarea>
+            {status === 'SUCCESS' ? (
+              <p>Thanks!</p>
+            ) : (
+              <button className="form-button">SUBMIT</button>
+            )}
+            {status === 'ERROR' && <p>Oops! Retry.</p>}
+          </form>
+        </div>
+      </Layout>
+    )
+  }
+
+  submitForm(ev) {
+    ev.preventDefault()
+    const form = ev.target
+    const data = new FormData(form)
+    const xhr = new XMLHttpRequest()
+    xhr.open(form.method, form.action)
+    xhr.setRequestHeader('Accept', 'application/json')
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        form.reset()
+        this.setState({ status: 'SUCCESS' })
+      } else {
+        this.setState({ status: 'ERROR' })
+      }
+    }
+    xhr.send(data)
+  }
+}
