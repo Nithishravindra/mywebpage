9173ffbe68723c65a84a7a32ab289ddc275d463d
nithish ravindra <nithishravindra8@gmail.com>
2020-07-04 23:26:31 +0530

    fixed typo in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,14 +8,13 @@ const Contact = () => (
       <p>
         I'm Nithish Ravindra currently pursuing B.E in Computer Science from
         Vivesvaraya Technological University(VTU). It's been few months since I
-        got into web development. Started with HTML, CSS and Javascript. I've
+        got into web development. Started with HTML, CSS and Javascript. I have
         picked ReactJS to help me through front-end. I've been also working on
         Backend Utilities like NodeJS, Python3, SQL and MongoDB to enhance
-        Full-Stack services. You can view some of my work in{' '}
-        <Link to="/projects">Projects</Link> section of this site.
+        Full-Stack services. You can see some of my work in{' '}
+        <Link to="/projects">Project</Link> section of this site.
       </p>
     </div>
-    <div></div>
   </Layout>
 )
 
